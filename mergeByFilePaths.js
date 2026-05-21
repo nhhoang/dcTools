e8781b00d260dc25aba491276f8f6919d164171f
Hoàng Nguyễn Huy [137] <hoangnh@geargames.com>
2026-05-21 16:29:31 +0700

    fixx

--- mergeByFilePaths.js
+++ mergeByFilePaths.js
@@ -30,11 +30,13 @@ const { execSync } = require('child_process');
 // Absolute path to the file containing the list of CLs
 const CHANGE_LIST_FILE = path.resolve(__dirname, 'changelists.txt');
 const FILE_PATHS = [
-"//dcwc/Gear_Character_Development_Config/excel/Language/LocalizationActivityUI.csv", 
-"//dcwc/Gear_Character_Development_Config/excel/Language/LocalizationCommonDesc.csv", 
-"//dcwc/Gear_Character_Development_Config/excel/Language/SourceCommonDesc.csv", 
-"//dcwc/Gear_Character_Development_Config/excel/TaskActivity.csv", 
-"//dcwc/Gear_Character_Development_Config/excel/TaskInfo.csv", 
+"//dcwc/Gear_Character_Development_Config/excel/AllyInfo.csv", 
+"//dcwc/Gear_Character_Development_Config/excel/Skill.csv", 
+"//dcwc/Gear_Character_Development_Config/excel/Profile.csv", 
+"//dcwc/Gear_Character_Development_Config/excel/ShareImage.csv", 
+"//dcwc/Gear_Character_Development_Config/excel/Enemies/EnemyEventBoss.csv", 
+"//dcwc/Gear_Character_Development_Config/excel/Skills/SkillEvent.csv", 
+"//dcwc/Gear_Character_Development_Config/excel/AllyCharacter.csv", 
 ]
 
 // Absolute path to the 2 Workspaces
